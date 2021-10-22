0a2c4a3aac221451590b8caff4d0a673690bbacb
unknown <emanuele.zini11@gmail.com>
2021-10-22 10:41:46 +0200

    added build for github pages test

---
+++ build/precache-manifest.2a5bbee32ee702a6f358321c8c4e93ba.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "82b47238df3c698c6b07",
+    "url": "/static/css/main.b9c7160e.chunk.css"
+  },
+  {
+    "revision": "82b47238df3c698c6b07",
+    "url": "/static/js/main.f5171ce3.chunk.js"
+  },
+  {
+    "revision": "b1da8d5ac50f30394f98",
+    "url": "/static/js/runtime~main.7bf7ad2c.js"
+  },
+  {
+    "revision": "b943a6d6335922afb651",
+    "url": "/static/js/2.dfcdd50a.chunk.js"
+  },
+  {
+    "revision": "5e218f672bc486ed9f93",
+    "url": "/static/css/3.30db812a.chunk.css"
+  },
+  {
+    "revision": "5e218f672bc486ed9f93",
+    "url": "/static/js/3.96398408.chunk.js"
+  },
+  {
+    "revision": "8345becbc48913250797",
+    "url": "/static/css/4.2ff81fc7.chunk.css"
+  },
+  {
+    "revision": "8345becbc48913250797",
+    "url": "/static/js/4.f92737c5.chunk.js"
+  },
+  {
+    "revision": "8c9fcbb672ba6c473e6d",
+    "url": "/static/css/5.2ff81fc7.chunk.css"
+  },
+  {
+    "revision": "8c9fcbb672ba6c473e6d",
+    "url": "/static/js/5.e6076d88.chunk.js"
+  },
+  {
+    "revision": "dbaf8d25bc84ff903841",
+    "url": "/static/css/6.2ff81fc7.chunk.css"
+  },
+  {
+    "revision": "dbaf8d25bc84ff903841",
+    "url": "/static/js/6.facfe90a.chunk.js"
+  },
+  {
+    "revision": "ad6c912593cb7eb4e79e",
+    "url": "/static/css/7.8ee3b234.chunk.css"
+  },
+  {
+    "revision": "ad6c912593cb7eb4e79e",
+    "url": "/static/js/7.73924c0e.chunk.js"
+  },
+  {
+    "revision": "c505364ea43d0d3073c1c2657454fa65",
+    "url": "/index.html"
+  }
+];
